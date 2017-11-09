feeec0e2991ee5b0650bff133f5a7094c8fa0086
Tang Her <bher87@gmail.com>
2017-11-09 15:02:52 -0500

    Hey Gia, I finally got the database down for the registration.php. All it needs now are the designs.

--- registration.php
+++ registration.php
@@ -3,19 +3,26 @@
 if(isset($_POST['submit'])) {
 
 $username = $_POST['username'];
-$password = $_POST['password'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$middlename = $_POST['middlename'];
+$gender = $_POST['gender'];
+$sport = $_POST['sport'];
+$classification = $_POST['classification'];
+$moreinfo = $_POST['moreinfo'];
 
-
-$connection = mysqli_connect('localhost', 'root', 'root', 'project2');
+$connection = mysqli_connect('localhost', 'root', 'root', 'project2db');
 
 	if($connection) {
-		echo "We are connected";
+		echo "We are connected ";
 	} else {
 		die("Database connection failed");
 	}
 	
-	$query = "INSERT INTO users(username, password) ";
-	$query .= "VALUES ('$username', '$password')";
+	$query = "INSERT INTO registration(username, firstname, lastname, middlename, 
+	gender, sport, classification, moreinfo) ";
+	$query .= "VALUES ('$username', '$firstname', '$lastname', '$middlename', '$gender', 
+	'$sport', '$classification', '$moreinfo')";
 	
 	$result = mysqli_query($connection, $query);
 	
@@ -24,94 +31,107 @@ $connection = mysqli_connect('localhost', 'root', 'root', 'project2');
 	}
 
 }
-
 ?>
 
-<html xmlns = "http://www.w3.org/1999/xhmtl">
+
+
+<!DOCYTYPE html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
+	
+<title>SportsPage</title>
+
+
 <link rel="stylesheet"
 href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-<title> Welcome to ShowSkill </title>
-	<h1> CSC 4320: Web Programming </h1>
-		<center>
-		<h1> ShowSkill </h1>
-		<h3> -- Athlete & Coach Oriented Social Networking Service -- </h3>
-		<h3> -- Register Here -- </h3><br />
-		</center>
-	</head>
+</head>
 
-	<body>
-		<center>
-		 <form action="https://grid.cs.gsu.edu/~cfrederick/csc4370/submit2login.php" 
-		method="get">
-		<table border="0">
-			<tr>
-				<td width="30%" align="middle"> Username </td>
-				<td><input type="text" name="username"/> (Your email)</td>
-			</tr>
-			<tr>
-				<td width="30%" align="middle"> First Name </td>
-				<td><input type="password" name="password"/></td>
-			</tr>
-			<tr>
-				<td width="30%" align="middle"> Last Name </td>
-				<td><input type="password" name="lastname"/></td>
-			</tr>
-			<tr>
-				<td width="30%" align="middle"> Middle Name </td>
-				<td><input type="password" name="middlename"/></td>
-			</tr>
-			<tr>
-				<td width="30%" align="middle"> Gender </td>
-				<td><select name="gender">
-						<option>Male</option>
-						<option>Female</option>
-					</select></td>
-			</tr>
-			<tr>
-				<td width="30%" align="middle"> Sport </td>
-				<td><input type="password" name="sport"/></td>
-			</tr>
-			<tr>
-				<td width="30%" align="middle"> Classification </td>
-				<td><select name="classification">
-						<option>Athlete</option>
-						<option>Coach</option>
-					</select></td>
-			</tr>
-			<tr>
-				<td width="30%" align="middle"> More Info </td>
-				<td><input type="password" name="password"/></td>
-			</tr>
-			
-		</table><br />
+
+
+<body>
+<center>
+<table border="0" width="40%">
+<div class="container">
+<form action="registration.php" method="post" name="Form" onsubmit="" action="">
+	
+	<tr>
+		<td>
+			<div class="form-group">
+			<label for="username">Username</label>
+			<input type="text" name="username" class="form-control" length="50" required="">
+		</td>
+	</tr></div>
 		
-		<table border=0>
-			
-			<tr>
-				<td><input class="btn btn-primary" type="submit" value="Submit"/></td>
-			</tr>
-		</table>
-		</form>
-		</center><br /><br />
-		
-		
-		<p1> Links to: </p1>
-				<br />
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment2_1.html">assignment2_1.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment2_2.html">assignment2_2.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment3_1.html">assignment3_1.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment3_2.html">assignment3_2.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment4.html">assignment4.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment5_1.html">assignment5_1.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment5_2.html">assignment5_2.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment6.html">assignment6.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment7.php">assignment7.php</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/Conways_GOL.html">Conways_GOL.html</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment8.php">assignment8.php</a></b></li>
-    		<li><b><a href="http://codd.cs.gsu.edu/~bher1/assignment9.xml">assignment9.xml</a></b></li>
-    			<br />
-		<p2> Click <a href="https://validator.w3.org">Here</a> to
-		validate my XHTML. </p2>
-	</body>
+	<tr>
+		<td>
+			<div class="form-group">
+			<label for="firstname">First Name</label>
+			<input type="text" name="firstname" class="form-control" length="50" required="">
+		</td>
+	</tr></div>
+	
+	<tr>		
+		<td>
+			<div class="form-group">
+			<label for="lastname">Last Name</label>
+			<input type="text" name="lastname" class="form-control" length="50" required="">
+		</td>
+	</tr></div>
+	
+	<tr>	
+		<td>
+			<div class="form-group">
+			<label for="middlename">Middle Name</label>
+			<input type="text" name="middlename" class="form-control" length="50" required="">
+		</td>
+	</tr></div>
+	
+	<tr>
+		<td><div class="form-group">
+			<label for="gender">Gender</label>
+			<select name="gender" class="form-control" length="7" required="">
+				<option></option>
+				<option>Male</option>
+				<option>Female</option>
+			</select></td>
+	</tr></div>
+	
+	<tr>
+		<td>
+			<div class="form-group">
+			<label for="sport">Sport</label>
+			<input type="text" name="sport" class="form-control" length="50" required="">
+		</td>
+	</tr></div>
+	
+	<tr>
+		<td><div class="form-group">
+			<label for="classification">Classification</label>
+			<select name="classification" class="form-control" length="15" required="">
+				<option></option>
+				<option>Athlete</option>
+				<option>Coach</option>
+			</select></td>
+	</tr></div>
+	
+	<tr>
+		<td>
+			<div class="form-group">
+			<label for="moreinfo">More Info</label>
+			<input type="text" name="moreinfo" class="form-control">
+		</td>
+	</tr>
+	<tr>
+		<td align="center">
+			<input class="btn btn-primary" type="submit" name="submit" value="Submit">
+		</td>
+	</tr>
+	</div>
+
+</form>
+</div>
+</table>
+</center>
+</body>
 </html>
